ea5456cde84179b044ac0a1472d4e5950347fc6c
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-01 15:39:11 -0400

    Added support for mdbreact.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,21 @@
  */
 
 // You can delete this file if you're not using it
+
+// This adds support for MDBReact
+
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "bootstrap-css-only/css/bootstrap.min.css"
+import "./src/styles/scss/mdb.scss"
+
+// Displays a message when a service worker updates
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Property Listing Page`,
+    description: `A single page application for single property listings.`,
+    author: `@zlutfi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,11 +15,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `property-listing-page`,
+        short_name: `property`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,

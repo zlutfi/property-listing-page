eb932cf20af24b2b8898cfa82952f2f10a509e3f
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-03 13:10:46 -0400

    Added hero component.

---
+++ src/components/hero.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { MDBView, MDBMask } from "mdbreact"
+
+const Hero = () => {
+  return (
+    <>
+      <MDBView>
+        <div className="bg">
+          {/* <MDBMask
+            className="flex-center"
+            overlay="white-slight"
+            pattern={1}
+          ></MDBMask> */}
+        </div>
+      </MDBView>
+    </>
+  )
+}
+export default Hero

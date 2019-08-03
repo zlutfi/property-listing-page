5b481df1180ccdaad291a532dc1665b0ce05616e
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-03 13:11:49 -0400

    Updated to support new components.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
 
+// Page Components
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Hero from "../components/hero"
+import Gallery from "../components/gallery"
+import Main from "../components/main"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Hero />
+    <SEO title="1682 Rollins Brooke Ct" />
+    <Main />
+    <Gallery />
   </Layout>
 )
 

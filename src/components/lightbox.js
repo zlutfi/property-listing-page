61759c6777ce3c1d2e328c6037d793164402d5b8
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-08-05 01:19:56 -0400

    Added data.

---
+++ src/components/lightbox.js
@@ -0,0 +1,107 @@
+import React from "react"
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBAnimation,
+  MDBView,
+  MDBMask,
+  MDBIcon,
+} from "mdbreact"
+import Lightbox from "react-image-lightbox"
+
+class LightboxPage extends React.Component {
+  state = {
+    photoIndex: 0,
+    isOpen: false,
+    images: this.props.data,
+  }
+
+  renderImages = () => {
+    let photoIndex = -1
+    const { images } = this.state
+
+    return images.map(imageSrc => {
+      photoIndex++
+      const privateKey = photoIndex
+      // const captions = [
+      //   "Kitchen",
+      //   "Master Bedroom",
+      //   "Master Bathroom",
+      //   "Dining Room",
+      //   "Alternate Kitchen View",
+      //   "Guest Bedroom",
+      // ]
+      return (
+        <MDBCol md="4" className="mb-3">
+          <MDBAnimation type="fadeIn" delay={"0.2" * photoIndex + "s"} reveal>
+            <figure className=" z-depth-1">
+              <div
+                onClick={() =>
+                  this.setState({ photoIndex: privateKey, isOpen: true })
+                }
+              >
+                <MDBView hover zoom>
+                  <img
+                    key={photoIndex}
+                    src={imageSrc}
+                    alt="Gallery"
+                    className="img-fluid"
+                  />
+                  <MDBMask overlay="black-light" className="flex-center">
+                    <p className="text-center lead text-white font-weight-bold">
+                      <MDBIcon icon="search-plus" className="mr-2" />
+                      {/* {captions[photoIndex]} */}
+                    </p>
+                  </MDBMask>
+                </MDBView>
+              </div>
+            </figure>
+          </MDBAnimation>
+        </MDBCol>
+      )
+    })
+  }
+
+  render() {
+    const { photoIndex, isOpen, images } = this.state
+    return (
+      <>
+        <div className="lightbox gallery container-fluid py-5">
+          <MDBContainer>
+            <MDBRow>
+              <MDBCol className="text-center text-md-left">
+                <h2 className="h2-responsive mb-5 font-weight-bold">
+                  Property Photos
+                </h2>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow>{this.renderImages()}</MDBRow>
+          </MDBContainer>
+        </div>
+        {isOpen && (
+          <Lightbox
+            mainSrc={images[photoIndex]}
+            nextSrc={images[(photoIndex + 1) % images.length]}
+            prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+            imageTitle={photoIndex + 1 + "/" + images.length}
+            enableZoom={false}
+            onCloseRequest={() => this.setState({ isOpen: false })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + images.length - 1) % images.length,
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: (photoIndex + 1) % images.length,
+              })
+            }
+          />
+        )}
+      </>
+    )
+  }
+}
+
+export default LightboxPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,52 @@
 import React from "react"
-
+import { graphql } from "gatsby"
 // Page Components
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Carousel from "../components/carousel"
-import Gallery from "../components/gallery"
+import LightboxPage from "../components/lightbox"
+
 import Main from "../components/main"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
-    <Carousel />
+    <Carousel
+      data={data.allFile.edges.map(img => {
+        return img.node.childImageSharp.fluid
+      })}
+    />
     <SEO title="1682 Rollins Brooke Ct" />
     <Main />
-    <Gallery />
+    <LightboxPage
+      data={data.allFile.edges.map(img => {
+        return img.node.childImageSharp.fluid.src
+      })}
+    />
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allFile(
+      filter: {
+        sourceInstanceName: { eq: "images" }
+        relativeDirectory: { eq: "gallery" }
+      }
+      limit: 6
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxHeight: 1080, maxWidth: 1920, cropFocus: CENTER) {
+              src
+              srcSet
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
